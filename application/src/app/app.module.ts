e8d036e40caaa67bc9e4fff5fd804ca0afbe7a60
Christian Lyon <christiandoescoding@gmail.com>
2024-01-05 20:57:26 -0600

    facing conflicts in routing with app.module.ts and app.routes.ts

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router'; 
 import { AppComponent } from './app.component';
+import { routes } from './app.routes'
+
+const routes: Routes = [
+
+]
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
       BrowserModule,
       HttpClientModule,
-      // other imports
+      RouterModule.forRoot(routes)
     ],
     providers: [],
     bootstrap: [AppComponent]
